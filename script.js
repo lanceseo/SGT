67b947cae8bf608d0531be2b7f5ec49aa1014e75
lanceseo <lance9980@gmail.com>
2016-03-17 21:22:04 -0700

    OO updated

--- script.js
+++ script.js
@@ -2,32 +2,43 @@
 
 var myStorage = localStorage;
 
+// sample data
+// var studentTest = [
+//     {'id':1, 'name':'Randy', 'course':'Politics', 'grade':55},
+//     {'id':2, 'name':'John', 'course':'Math', 'grade':66},
+//     {'id':3, 'name':'Sandy', 'course':'Physics', 'grade':98}
+// ]
+// myStorage.setItem('student1', JSON.stringify(studentTest));
+
+
 $("document").ready(function() {
     $(".addData").on("click", function() {
-        // a_DOM.getInput(a_Student);
-        a_Student = {[1, "James", "Politics", 99], [2, "Randy", "Science", 77]};
-        console.log(a_Student);
-        //a_Storage.addData(a_Student);
-        // a_SGT_Storage.addData(a_Student.id, a_Student.name, a_Student.course, a_Student.grade);
+        a_DOM.getInput(a_Student);
+        a_Storage.getData(a_SGT);
+        //console.log("New ID: " + a_SGT.newID());
+        a_Storage.addData(a_Student, a_SGT);        
+        
+        // getData again to populate the Table
+        a_Storage.getData(a_SGT);
+        a_DOM.populate(a_SGT.studentArray);
     });
+
     $(".getData").on("click", function() {
-        //a_Storage.getData(a_SGT);
-        a_DOM.populate(myStorage.getItem('student1'));
+        a_Storage.getData(a_SGT);
+        a_DOM.populate(a_SGT.studentArray);
     });
 });
 
-// // callback function for API call
-// function loadData(data, obj1, obj2) {
-//     // console.log(data, obj1, obj2);
-//     obj1.setStudentArray(data);
-//     obj2.populate(obj1.studentArray);
-// }
 
 var SGT = function() {
     var self = this;
-    self.setStudentArray = function(serverData) {
-        self.studentArray = serverData;
+    self.studentArray = [];
+    self.newID = function() {
+        return self.studentArray.length + 1;
     }
+    // self.setStudentArray = function(storageData) {
+    //     self.studentArray = storageData;
+    // }
 };
 
 var Student = function() {
@@ -40,14 +51,14 @@ var Student = function() {
 
 var SGT_DOM = function() {
     var self = this;
-    self.populate = function(sData) {
-        var sDataLength = sData.length;
+    self.populate = function(studentData) {
+        var sDataLength = studentData.length;
         for (var i=0; i<sDataLength; i++) {
             var tr = $("<tr>");
-            var tdId = $("<td>").text(sData[i].id);
-            var tdName = $("<td>").text(sData[i].name);
-            var tdCourse = $("<td>").text(sData[i].course);
-            var tdGrade =  $("<td>").text(sData[i].grade);
+            var tdId = $("<td>").text(studentData[i].id);
+            var tdName = $("<td>").text(studentData[i].name);
+            var tdCourse = $("<td>").text(studentData[i].course);
+            var tdGrade =  $("<td>").text(studentData[i].grade);
             tr.append(tdId, tdName, tdCourse, tdGrade);
             $(".student-list tbody").append(tr);
         }
@@ -57,7 +68,7 @@ var SGT_DOM = function() {
         var sCourse = $("#course").val();
         var sGrade = $("#studentGrade").val();
         console.log(sName, sCourse, sGrade);
-        newStudent.id = 1;
+        //newStudent.id = 1;
         newStudent.name = sName;
         newStudent.course = sCourse;
         newStudent.grade = sGrade;
@@ -67,12 +78,12 @@ var SGT_DOM = function() {
 var SGT_Storage = function() {
     var self = this;
     self.getData = function(newSGT) {
-        newSGT.studentArray = myStorage.getItem('student1');
+        newSGT.studentArray = JSON.parse(myStorage.getItem('student1'));
     }
-
-    self.addData = function(a_Student) {
-        myStorage.setItem('student1', JSON.stringify(a_Student));
-        console.log( JSON.parse(myStorage.getItem('student1')));
+    self.addData = function(newStudent, newSGT) {
+        newStudent.id = newSGT.newID();
+        newSGT.studentArray.push(newStudent);
+        myStorage.setItem('student1', JSON.stringify(newSGT.studentArray));
     }
 }
 
@@ -82,40 +93,3 @@ var a_SGT = new SGT();
 var a_DOM = new SGT_DOM();
 var a_Storage = new SGT_Storage();
 
-
-// var a_API = new SGT_API();
-
-
-// var SGT_API = function() {
-//     var self = this;
-//     var apiKey = '2VSlnQzAoX';
-//     self.getDataFromServer = function(obj1, obj2) {
-//         $.ajax({
-//             dataType: 'json',
-//             method: 'post',
-//             data: {
-//                 api_key: apiKey
-//             },
-//             url: 'http://s-apis.learningfuze.com/sgt/get',
-//             success: function(result) {
-//                 loadData(result.data, obj1, obj2);
-//             }
-//         });
-//     };
-//     self.addDataToServer = function(sName, sCourse, sGrade) {
-//         $.ajax({
-//             dataType: 'json',
-//             method: 'post',
-//             data: {
-//                 api_key: apiKey,
-//                 name: sName,
-//                 course: sCourse,
-//                 grade: sGrade
-//             },
-//             url: 'http://s-apis.learningfuze.com/sgt/create',
-//             success: function(result) {
-//                 console.log("New ID: ", result.new_id);
-//             }
-//         });
-//     };
-// };
