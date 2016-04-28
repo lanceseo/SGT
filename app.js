8a1d488de6a8ab66d7dfc8decba83278c8489053
lanceseo <lance9980@gmail.com>
2016-04-27 19:51:41 -0700

    all scopes defined #10

--- app.js
+++ app.js
@@ -1,22 +1,26 @@
 var app = angular.module('sgtapp',[]);
 
-// CRUD controller
-app.controller('dataController', function(){
-	var self = this;
+// CRUD operation
+app.service('storageOper', function() {
 	var locStorage = localStorage;
-	this.sArray = [];
-
 	this.getStorage = function() {
 		console.log(JSON.parse(locStorage.getItem('sData')));
 		return JSON.parse(locStorage.getItem('sData'));
 	};
 	this.setStorage = function(sArray) {
 		locStorage.setItem('sData', JSON.stringify(sArray));
+		console.log('localStorage Set');
 	};
 	this.removeStorage = function() {
 		locStorage.removeItem('sData');
+		console.log('localStorage wiped');
 	};
+});
 
+// Click controller
+app.controller('dataController', function(storageOper){
+	var self = this;
+	this.sArray = [];
 	this.newID = function() {
         if (self.sArray.length > 0) {
             return self.sArray[self.sArray.length-1].id+1;
@@ -26,31 +30,30 @@ app.controller('dataController', function(){
     };
 
 	this.getData = function() {
-			self.sArray = self.getStorage();
+		self.sArray = storageOper.getStorage();
 	};
 	this.addData = function(sname, scourse, sgrade) {
-			var newStudent = {};
-			newStudent.id = self.newID();
-			newStudent.name = sname;
-			newStudent.course = scourse;
-			newStudent.grade = parseInt(sgrade);
-			self.sArray.push(newStudent);
-			self.setStorage(self.sArray);
+		var newStudent = {};
+		newStudent.id = self.newID();
+		newStudent.name = sname;
+		newStudent.course = scourse;
+		newStudent.grade = parseInt(sgrade);
+		self.sArray.push(newStudent);
+		storageOper.setStorage(self.sArray);
 	};
 	this.deleteData = function(sid) {
 		for (var i=0; i<self.sArray.length; i++) {
 			if (self.sArray[i].id === parseInt(sid)) {
 				self.sArray.splice(i,1);
-				self.setStorage(self.sArray);
+				storageOper.setStorage(self.sArray);
 				return;
 			} else if (i === self.sArray.length-1) {
-				console.log('not found');
+				console.log('ID not found');
 			}
 		}
 	};
 	this.wipeData = function() {
-		self.removeStorage();
-		console.log('data wiped');
+		storageOper.removeStorage();
 	};
 
 });
